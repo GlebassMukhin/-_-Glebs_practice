f494983f21bd605e8420da002f19b714f022d7a1
p_sh <167682701+GlebassMukhin@users.noreply.github.com>
2025-08-24 00:25:09 +0300

    Add files via upload

---
+++ wrong_combine.py
@@ -0,0 +1,14 @@
+list_1 = [1, 2, 56, 34, 241]
+list_2 = [2, 56, 34, 123, 6]
+def combine_and_unique():
+    z = 0
+    list_1 = [1, 2, 56, 34, 241]
+    list_2 = [2, 56, 34, 123, 6]
+    list_3 = []
+    for number in range:
+        if number == list_1 and number == list_2:
+            z += 1
+            print(z)
+    else:
+        list_3. append(number)
+    print(list_3)
